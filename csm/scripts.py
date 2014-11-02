189f35f675875634e2f16a17aa5c03bc2f1c13d8
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-11-02 16:48:53 -0300

    Initial commit

---
+++ csm/scripts.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import logging
+
+
+logger = logging.getLogger(__name__) #pylint: disable=invalid-name
+
+
+def csm():
+    raise NotImplementedError()
