9c84a39f0c2304bb5986de280871814c3f48a9ba
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-11-09 17:56:05 -0300

    -s/--server argument

--- csm/scripts.py
+++ csm/scripts.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import argparse
 import json
 import logging
 
@@ -11,7 +12,17 @@ from .columns import get_system_columns
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
+def _get_csm_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server",
+                        action="store", default='localhost',
+                        help=("Server to retrieve schema from. Defaults to "
+                              "localhost"))
+    return parser
+
+
 def csm():
-    print "Current system columns:"
-    columns = get_system_columns()
+    options = _get_csm_argument_parser().parse_args()
+    print "Schema for '{}'".format(options.server)
+    columns = get_system_columns(options.server)
     print json.dumps(columns, indent=1)
